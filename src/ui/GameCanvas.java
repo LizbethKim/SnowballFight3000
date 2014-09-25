7202acf604af883e5aebfc0944c539b73f47bf16
RyanBurnell <ryan.burnell2@gmail.com>
2014-09-25 17:05:14 +1200

    Added Inventory bar and show/hide button to the HUD

--- src/ui/GameCanvas.java
+++ src/ui/GameCanvas.java
@@ -7,6 +7,8 @@ import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -22,13 +24,22 @@ public class GameCanvas extends Canvas {
 	public static final String IMAGE_PATH = "src/ui/HUDAssets/";
 	private static final Image healthBar = loadImage("HealthBar.png");
 	private static final Image healthBase = loadImage("HealthBarBase.png");
+	private static final Image inventorySlot = loadImage("InventorySlot.png");
+	private static final Image hideItems = loadImage("HideItems.png");
+	private static final Image showItems = loadImage("ShowItems.png");
+	
+	private static final double showHideProportion = 1.0/20.0;
+	
 	
 	private final Client client;
+	private boolean inventoryHidden = false;
+	
 
 	public GameCanvas(Client cl) {
 		this.client = cl;
 		super.setCursor(new Cursor(Cursor.HAND_CURSOR));
-
+		setupListeners();
+		
 		GraphicsEnvironment env = GraphicsEnvironment
 				.getLocalGraphicsEnvironment();
 		HashSet<String> availableNames = new HashSet();
@@ -43,21 +54,74 @@ public class GameCanvas extends Canvas {
 	 */
 	public void paint(Graphics g) {
 		paintHealthBar(g);
+		paintShowHideButton(g);
+		if(!inventoryHidden){
+		paintInventory(g);
+		}
+	}
+	
+	
+	
+	private void paintShowHideButton(Graphics g){
+		final int width = (int)(this.getWidth()*showHideProportion);
+		final int height = width*2;
+		final int xPos = 0;
+		final int yPos = this.getHeight()-height;
+		Image showHideButton;
+		if(inventoryHidden){
+		showHideButton = showItems.getScaledInstance(width, height, 0);
+		} else {
+		showHideButton = hideItems.getScaledInstance(width, height, 0);
+		}	
+		g.drawImage(showHideButton, xPos, yPos, null, null);
+	}
+	
+	private void paintInventory(Graphics g){
+		final int inventoryNumber = 10;
+		int xPos = (int)(this.getWidth()*showHideProportion);
+		final int size = (this.getWidth()-xPos)/inventoryNumber;
+		final int yPos = this.getHeight()-size;
+		
+		Image slot = inventorySlot.getScaledInstance(size, size, 0);
+		for(int i = 0; i < inventoryNumber; i++){
+			g.drawImage(slot, xPos, yPos, null, null);
+			xPos += size;
+		}
 	}
 	
 	private void paintHealthBar(Graphics g){
-		final int xPos = this.getWidth()/10;
-		final int yPos = this.getHeight()/10;
-		final int width = this.getWidth()/5;
-		final int height = this.getHeight()/14;
-		final int healthXPos = xPos+(height/4);
-		final int healthYPos = yPos+(width/4);
+		final int healthRemaining = 1;
+		
+		final int xPos = this.getWidth()/20;
+		final int yPos = this.getHeight()/20;
+		final int width = this.getWidth()/3;
+		final int height = this.getHeight()/11;
+		final int healthXPos = xPos+(width/4);
+		final int healthYPos = yPos+(height/4);
+		final int xDifference = healthXPos-xPos;
+		final int healthWidth = (width - xDifference)/healthRemaining;
+		final int healthHeight = height*3/5;
 		
 		
 		Image base = healthBase.getScaledInstance(width, height, 0);
-		Image bar = healthBar.getScaledInstance(width, height, 0);
+		Image bar = healthBar.getScaledInstance(healthWidth, healthHeight, 0);
 		g.drawImage(base, xPos, yPos, null, null);
 		g.drawImage(bar, healthXPos, healthYPos, null, null);
+	}
+	
+	private void setupListeners(){
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dealWithClick(e);
+			}
+		});
+	}
+	
+	private void dealWithClick(MouseEvent e){
+		int x = e.getX();
+		int y = e.getY();
+		//if()
 	}
 	
 	/**
