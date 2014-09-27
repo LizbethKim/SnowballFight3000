5869766ce505c1823e6e513b2c48f9826291f77e
RyanBurnell <ryan.burnell2@gmail.com>
2014-09-27 15:25:17 +1200

    Drawn health bar now reflects player health

--- src/ui/HUDPanel.java
+++ src/ui/HUDPanel.java
@@ -24,8 +24,6 @@ import javax.swing.JPanel;
 
 import client.Client;
 
-
-
 public class HUDPanel extends JPanel {
 	public static final String IMAGE_PATH = "src/ui/HUDAssets/";
 	private static final Image healthBar = loadImage("HealthBar.png");
@@ -33,20 +31,18 @@ public class HUDPanel extends JPanel {
 	private static final Image inventorySlot = loadImage("InventorySlot.png");
 	private static final Image hideItems = loadImage("HideItems.png");
 	private static final Image showItems = loadImage("ShowItems.png");
-	
-	private static final double showHideXProportion = 1.0/20.0;
-	private static final double inventoryYProportion = 1.0/10.0;
-	
-	
-	
+
+	private static final double showHideXProportion = 1.0 / 20.0;
+	private static final double inventoryYProportion = 1.0 / 10.0;
+	private static final double maxHealth = 100.0;
+
 	private final Client client;
 	private boolean inventoryHidden = false;
-	
 
 	public HUDPanel(Client cl) {
 		this.client = cl;
 		setupListeners();
-		//this.set
+		// this.set
 		GraphicsEnvironment env = GraphicsEnvironment
 				.getLocalGraphicsEnvironment();
 		HashSet<String> availableNames = new HashSet();
@@ -60,112 +56,114 @@ public class HUDPanel extends JPanel {
 	 * draws the board and players
 	 */
 	public void paint(Graphics g) {
-		System.out.println("WIDTH:     "+getWidth());
-		System.out.println("HEIGHT:     "+getHeight());
+		System.out.println("WIDTH:     " + getWidth());
+		System.out.println("HEIGHT:     " + getHeight());
 		paintHealthBar(g);
 		paintShowHideButton(g);
-		if(!inventoryHidden){
-		paintInventory(g);
+		if (!inventoryHidden) {
+			paintInventory(g);
 		}
 	}
-	
-	
-	
-	private void paintShowHideButton(Graphics g){
-		final int width = (int)(this.getWidth()*showHideXProportion);
-		final int height = getHeight()-getInventoryYPos();
+
+	private void paintShowHideButton(Graphics g) {
+		final int width = (int) (this.getWidth() * showHideXProportion);
+		final int height = getHeight() - getInventoryYPos();
 		final int xPos = 0;
 		final int yPos = getInventoryYPos();
 		Image showHideButton;
-		if(inventoryHidden){
-		showHideButton = showItems.getScaledInstance(width, height, 0);
+		if (inventoryHidden) {
+			showHideButton = showItems.getScaledInstance(width, height, 0);
 		} else {
-		showHideButton = hideItems.getScaledInstance(width, height, 0);
-		}	
+			showHideButton = hideItems.getScaledInstance(width, height, 0);
+		}
 		g.drawImage(showHideButton, xPos, yPos, null, null);
 	}
-	
-	private void paintInventory(Graphics g){
+
+	private void paintInventory(Graphics g) {
 		final int inventoryNumber = 9;
 		int xPos = getShowHideWidth();
-		final int size = this.getHeight()-getInventoryYPos();
-		int fontSize = size/6;
+		final int size = this.getHeight() - getInventoryYPos();
+		int fontSize = size / 6;
 		Image slot = inventorySlot.getScaledInstance(size, size, 0);
-		for(int i = 0; i < inventoryNumber; i++){
+		for (int i = 0; i < inventoryNumber; i++) {
 			g.drawImage(slot, xPos, getInventoryYPos(), null, null);
 			g.setFont(new Font("Times", Font.PLAIN, fontSize));
-			int numberXPos = xPos + size/12;
-			int numberYPos = getInventoryYPos() + size/30 + fontSize;
+			int numberXPos = xPos + size / 12;
+			int numberYPos = getInventoryYPos() + size / 30 + fontSize;
 			g.setColor(Color.white);
-			g.drawString(""+(i+1), numberXPos, numberYPos);
+			g.drawString("" + (i + 1), numberXPos, numberYPos);
 			xPos += size;
 		}
 	}
-	
-	private void paintHealthBar(Graphics g){
-		final int healthRemaining = 1;
-		
-		final int xPos = this.getWidth()/20;
-		final int yPos = this.getHeight()/20;
-		final int width = this.getWidth()/3;
-		final int height = this.getHeight()/11;
-		final int healthXPos = xPos+(width/4);
-		final int healthYPos = yPos+(height/4);
-		final int xDifference = healthXPos-xPos;
-		final int healthWidth = (width - xDifference)/healthRemaining;
-		final int healthHeight = height*3/5;
-		
-		
+
+	private void paintHealthBar(Graphics g) {
+		final int xPos = this.getWidth() / 20;
+		final int yPos = this.getHeight() / 20;
+		final int width = this.getWidth() / 3;
+		final int height = this.getHeight() / 11;
+		final int healthXPos = xPos + (width / 4);
+		final int healthYPos = yPos + (height / 4);
+		final int xDifference = healthXPos - xPos;
+		final double healthProportion = client.getPlayer().getHealth() / maxHealth;
+		final int healthWidth = (int)((width - xDifference) * healthProportion);
+		final int healthHeight = height * 3 / 5;
+
 		Image base = healthBase.getScaledInstance(width, height, 0);
-		Image bar = healthBar.getScaledInstance(healthWidth, healthHeight, 0);
 		g.drawImage(base, xPos, yPos, null, null);
-		g.drawImage(bar, healthXPos, healthYPos, null, null);
+		if (healthWidth > 0) {
+			Image bar = healthBar.getScaledInstance(healthWidth, healthHeight,
+					0);
+			g.drawImage(bar, healthXPos, healthYPos, null, null);
+		}
+
 	}
-	
-	private void setupListeners(){
+
+	private void setupListeners() {
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				dealWithClick(e);
 			}
 		});
-		
+
 		addMouseMotionListener(new MotionListener());
-			
+
 	}
-	
-	private void updateCursor(MouseEvent e){
-		if(onShowHideButton(e.getX(), e.getY())){
+
+	private void updateCursor(MouseEvent e) {
+		if (onShowHideButton(e.getX(), e.getY())) {
 			super.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		} else {
 			super.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 		}
 	}
-	
-	private int getInventoryYPos(){
-		return this.getHeight()-(int)(this.getHeight()*inventoryYProportion);
+
+	private int getInventoryYPos() {
+		return this.getHeight()
+				- (int) (this.getHeight() * inventoryYProportion);
 	}
-	
-	private int getShowHideWidth(){
-		return (int)(this.getWidth()*showHideXProportion);
+
+	private int getShowHideWidth() {
+		return (int) (this.getWidth() * showHideXProportion);
 	}
-	
-	private boolean onShowHideButton(int x, int y){
-		return y>0 && x>0 && y > getInventoryYPos() && x < getShowHideWidth();
+
+	private boolean onShowHideButton(int x, int y) {
+		return y > 0 && x > 0 && y > getInventoryYPos()
+				&& x < getShowHideWidth();
 	}
-	
-	private void dealWithClick(MouseEvent e){
-		//RB: Fix mouse hover when window is resized
+
+	private void dealWithClick(MouseEvent e) {
+		// RB: Fix mouse hover when window is resized
 		System.out.println("dealing with click");
 		int x = e.getX();
 		int y = e.getY();
-		if(onShowHideButton(x,y)){
+		if (onShowHideButton(x, y)) {
 			System.out.println("show/hide");
 			inventoryHidden = !inventoryHidden;
 			repaint();
 		}
 	}
-	
+
 	/**
 	 * Load an image from the file system, using a given filename.
 	 * 
@@ -186,16 +184,16 @@ public class HUDPanel extends JPanel {
 		}
 
 	}
-	
-	private class MotionListener implements MouseMotionListener{
+
+	private class MotionListener implements MouseMotionListener {
 		@Override
-		public void mouseMoved(MouseEvent e){
+		public void mouseMoved(MouseEvent e) {
 			updateCursor(e);
 		}
-		
+
 		@Override
 		public void mouseDragged(MouseEvent arg0) {
-			//do nothing
+			// do nothing
 		}
 	}
 }
