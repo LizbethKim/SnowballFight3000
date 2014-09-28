e2d50585487c38ddcd253fc9147a0c9b19d4550d
Bryden Frizzell <frizzebryd@steamboat.ecs.vuw.ac.nz>
2014-09-28 15:40:05 +1300

    iI hate everythingD Made UpdateEvent an interfl

--- src/server/events/MoveEvent.java
+++ src/server/events/MoveEvent.java
@@ -1,11 +1,23 @@
 package server.events;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import gameworld.world.Location;
 
-public class MoveEvent {
-
-	public MoveEvent(Location l) {
+public class MoveEvent implements UpdateEvent {
+	int playerID;
+	Location location;
+	public MoveEvent(int playerID, Location l) {
+		this.playerID=playerID;
+		this.location=l;
 		// AUTO
+	}
+
+	@Override
+	public void writeTo(OutputStream out) throws IOException {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/server/events/TurnEvent.java
+++ src/server/events/TurnEvent.java
@@ -1,11 +1,26 @@
 package server.events;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import gameworld.world.Direction;
 
-public class TurnEvent {
+public class TurnEvent implements UpdateEvent {
 
-	public TurnEvent(Direction d) {
+	private Direction direction;
+	private int playerID;
+
+	public TurnEvent(int playerID, Direction d) {
 		// AUTO
+		this.playerID=playerID;
+		this.direction=d;
+	}
+
+	@Override
+	public void writeTo(OutputStream out) throws IOException {
+		// TODO Auto-generated method stub
+		out.write(0x02);
+		out.write(0x02);
 	}
 
 }
