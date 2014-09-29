f798efb9b50ad3edbe3adc2e9d67041a87a82c2a
Bryden Frizzell <bryden.frizzell@gmail.com>
2014-09-29 19:53:07 +1300

    Added lots of client receive code

--- src/server/Client.java
+++ src/server/Client.java
@@ -2,8 +2,10 @@ package server;
 
 import gameworld.world.Direction;
 import gameworld.world.Location;
+import gameworld.world.Player;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -12,7 +14,7 @@ import client.Updater;
 
 public class Client implements Runnable {
 	private final String host;
-	
+
 	private Updater updater;
 
 	private Socket connection;
@@ -30,44 +32,69 @@ public class Client implements Runnable {
 		}
 	}
 
-
 	@Override
 	public void run() {
-		while(true) {
+		while (true) {
 			try {
 				// debugging code
 				byte in = readFromSocket();
-				//move packet
-				if(in==0x01) {
+				// move player
+				if (in == 0x01) {
 					int id = readFromSocket();
-					int x = readFromSocket()+readFromSocket()>>8;
-					int y = readFromSocket()+readFromSocket()>>8;
-					updater.movePlayer(id, new Location(x,y));
+					int x = readFromSocket() + readFromSocket() >> 8;
+					int y = readFromSocket() + readFromSocket() >> 8;
+					updater.movePlayer(id, new Location(x, y));
 				}
-				//turn player
-				if(in==0x02) {
+				// turn player
+				else if (in == 0x02) {
 					int id = readFromSocket();
 					int dir = readFromSocket();
 					updater.turnPlayer(id, Direction.values()[dir]);
 				}
-				//create player
-				if(in==0x05) {
+				// create player
+				else if (in == 0x05) {
 					int id = readFromSocket();
-					updater.addPlayer(id);
+					String name = readString();
+					// still pretty pissed about this
+					updater.addPlayer(id, new Player(name, id));
+				}
+				// create local player
+				else if (in == 0x06) {
+					int id = readFromSocket();
+					// BF add updater function here
 				}
 				
+
 				System.out.println(in);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				return;
+			} catch(SocketClosedException e) {
+				System.out.println("Socket is closed...");
+				e.printStackTrace();
+				return;
 			}
 		}
 	}
-	
-	public void sendMove(Direction d) {
+
+	public void sendMove(Location l) {
 		try {
 			connection.getOutputStream().write(0x01);
+			connection.getOutputStream().write(l.x);
+			connection.getOutputStream().write(l.x>>8);
+			connection.getOutputStream().write(l.y);
+			connection.getOutputStream().write(l.y>>8);
+			connection.getOutputStream().flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendTurn(Direction d) {
+		try {
+			connection.getOutputStream().write(0x02);
 			connection.getOutputStream().write(d.ordinal());
 			connection.getOutputStream().flush();
 		} catch (IOException e) {
@@ -76,16 +103,41 @@ public class Client implements Runnable {
 		}
 	}
 
+
+	public void sendName(String name) {
+		try {
+			connection.getOutputStream().write(0x06);
+			connection.getOutputStream().write(name.length());
+			for(int i=0;i<name.length();i++) {
+				connection.getOutputStream().write(name.getBytes()[i]);
+			}
+			connection.getOutputStream().flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void startReceiving(Updater c) {
 		this.updater = c;
 		Thread t = new Thread(this);
 		t.start();
 	}
+	
+	private String readString() throws IOException, SocketClosedException {
+		String output = "";
+		int len = readFromSocket();
+		for(int i=0;i<len;i++) {
+			output = output + (char)readFromSocket();
+		}
+		return output;
+	}
 
-	private byte readFromSocket() throws IOException {
+	private byte readFromSocket() throws IOException, SocketClosedException {
+		//I hate java
 		int input = connection.getInputStream().read();
 		if (input == -1) {
-			throw new IOException("Socket closed");
+			throw new SocketClosedException();
 		}
 		return (byte) input;
 	}
