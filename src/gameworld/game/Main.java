045e2315ea22c1745447bd0972c8ece0da0631e9
Kelsey Jack <jackkels@gare-fare.ecs.vuw.ac.nz>
2014-10-01 09:44:07 +1300

    fixed args so no args works for main

--- src/gameworld/game/Main.java
+++ src/gameworld/game/Main.java
@@ -29,28 +29,31 @@ public class Main {
 	}
 
 	public static void main (String[] args) {
-		System.out.println(args[0]);
-		if (args[0].equals("server")) {
-			// KTC put an Updater in this function:
-			ServerGame g = new ServerGame(new Board());
-			Server server = new Server(g);
-			// start server connection accepting thread
-			new Thread(server).start();
-			// currently, the server hogs the current thread (this function runs a while(true) statement)
-			server.sendLoop();
-			// I think in theory, the server's main loop is basically the server recieve loop anyway, so this should be fine
-			// (since the tick stuff should happen in another thread anyway)
-			// the only caveat is a client probably can't be a server as well
-			// if you don't like this, I can change it and make it more multithreaded
-		} else if (args[0].equals("client")) {
-			Client client = new Client("127.0.0.1"); //connect to localhost for testing, you can put a different address here
-			Board b = new Board();	// This should be new from file - the first file to come through the network perhaps
-			ClientGame g = new ClientGame(b, client);
-			//this function creates a new thread for itself
+		if (args.length > 0) {
+			System.out.println(args[0]);
+			if (args[0].equals("server")) {
+				// KTC put an Updater in this function:
+				ServerGame g = new ServerGame(new Board());
+				Server server = new Server(g);
+				// start server connection accepting thread
+				new Thread(server).start();
+				// currently, the server hogs the current thread (this function runs a while(true) statement)
+				server.sendLoop();
+				// I think in theory, the server's main loop is basically the server recieve loop anyway, so this should be fine
+				// (since the tick stuff should happen in another thread anyway)
+				// the only caveat is a client probably can't be a server as well
+				// if you don't like this, I can change it and make it more multithreaded
+			} else if (args[0].equals("client")) {
+				Client client = new Client("127.0.0.1"); //connect to localhost for testing, you can put a different address here
+				Board b = new Board();	// This should be new from file - the first file to come through the network perhaps
+				ClientGame g = new ClientGame(b, client);
+				//this function creates a new thread for itself
 
-			ClientUpdater u = g.makeUpdater();
-			client.startReceiving(u);
-		} else { // debug single-player testing dev mode
+				ClientUpdater u = g.makeUpdater();
+				client.startReceiving(u);
+			}
+		}
+		else { // debug single-player testing dev mode
 
 			// KH, this is where I'll need dataloading methods
 
