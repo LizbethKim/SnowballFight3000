5c40e2cfbc87a7f4df88c4b74b9fd44271162bf8
Bryden Frizzell <frizzebryd@the-meeting-tree.ecs.vuw.ac.nz>
2014-10-04 12:54:14 +1300

    Added SendMapEvent

---
+++ src/server/events/SendMapEvent.java
@@ -0,0 +1,18 @@
+package server.events;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SendMapEvent implements UpdateEvent {
+
+	public SendMapEvent() {
+
+	}
+
+	@Override
+	public void writeTo(OutputStream out) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
