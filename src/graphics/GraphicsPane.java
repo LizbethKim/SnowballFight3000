1bd381a47b2cbf50dbaf4ef4a96a0f8882f43c65
Elizabeth Kim <eliz.kim91@gmail.com>
2014-10-04 13:29:45 +1300

    All four directions now working
    All four directions of views now work! Congratulations on the Math
    Lizzy!

--- src/graphics/GraphicsPane.java
+++ src/graphics/GraphicsPane.java
@@ -42,6 +42,12 @@ public class GraphicsPane extends JPanel {
 		case SOUTH:
 			drawSouth(currentBoard, currentObjects, g);
 			break;
+		case EAST:
+			drawEast(currentBoard, currentObjects, g);
+			break;
+		case WEST:
+			drawWest(currentBoard, currentObjects, g);
+			break;
 		default:
 			drawNorth(currentBoard, currentObjects, g);
 			break;
@@ -165,5 +171,121 @@ public class GraphicsPane extends JPanel {
 			}
 		}
 	}
+	
+	private void drawEast(Terrain[][] currentBoard,
+			Objects[][] currentObjects, Graphics g) {
+		for (int i = 0; i < currentBoard.length; i++) {
+			for (int j = 0; j < currentBoard[0].length; j++) {
+				double x = (i * 0.5 * width / currentBoard.length)
+						- (j * 0.5 * width / currentBoard.length)
+						+ (currentBoard.length / 1.75) * (int) width / 13;
+				double y = (i * 0.5 * height / (currentBoard[0].length * 1.5))
+						+ (j * 0.5 * height / (currentBoard[0].length * 1.5))
+						+ (currentBoard[0].length / 3) * (int) height / 21;
+				if (currentBoard[currentBoard.length - 1 - i][currentBoard[0].length
+						- 1 - j] != null)
+					g.drawImage(
+							currentBoard[j][currentBoard.length
+									- 1 - i].img, (int) x, (int) y,
+							(int) width / 10, (int) height / 15, null);
+				else {
+					System.out.println("THIS SHIT IS NULL NIGGA");
+				}
+			}
+		}
 
+		for (int i = 0; i < currentBoard.length; i++) {
+			for (int j = -1; j < currentBoard[0].length - 1; j++) {
+				if (currentObjects[j + 1][currentObjects[0].length
+						- 1 - i] != null) {
+					double x = (i * 0.5 * width / currentBoard.length)
+							- (j * 0.5 * width / currentBoard.length)
+							+ (currentBoard.length / 1.75)
+							* (int) width
+							/ 13
+							- (0.25 * width / currentBoard.length)
+							- (0.125 * currentObjects[j + 1][currentObjects.length - 1 - i].imgs[3]
+									.getWidth(null) * (width / startingWidth));
+					double y = (i * 0.5 * height / (currentBoard[0].length * 1.5))
+							+ (j * 0.5 * height / (currentBoard[0].length * 1.5))
+							+ (currentBoard[0].length / 3)
+							* (int) height
+							/ 21
+							+ (0.25 * height / currentBoard[0].length * 1.5)
+							- (currentObjects[j + 1][currentObjects.length
+									- 1 - i].imgs[0].getHeight(null)
+									* (height / startingHeight) - 0.5
+									* currentBoard[i][j + 1].img
+											.getHeight(null)
+									* (height / startingHeight));
+					System.out.println(currentObjects[j + 1][currentObjects.length - i - 1]);
+					g.drawImage(
+							currentObjects[j + 1][currentObjects.length
+									- i - 1].imgs[2],
+							(int) x,
+							(int) y,
+							(int) (currentObjects[j + 1][currentObjects.length
+									- 1 - i].imgs[0].getWidth(null) * (width / startingWidth)),
+							(int) (currentObjects[j + 1][currentObjects.length
+									- 1 - i].imgs[0].getHeight(null) * (height / startingHeight)),
+							null);
+				}
+			}
+		}
+	}
+	
+	private void drawWest(Terrain[][] currentBoard,
+			Objects[][] currentObjects, Graphics g) {
+		for (int i = 0; i < currentBoard.length; i++) {
+			for (int j = 0; j < currentBoard[0].length; j++) {
+				double x = (i * 0.5 * width / currentBoard.length)
+						- (j * 0.5 * width / currentBoard.length)
+						+ (currentBoard.length / 1.75) * (int) width / 13;
+				double y = (i * 0.5 * height / (currentBoard[0].length * 1.5))
+						+ (j * 0.5 * height / (currentBoard[0].length * 1.5))
+						+ (currentBoard[0].length / 3) * (int) height / 21;
+				if (currentBoard[currentBoard[0].length - 1 - j][i] != null)
+					g.drawImage(
+							currentBoard[currentBoard[0].length - 1 - j][i].img, (int) x, (int) y,
+							(int) width / 10, (int) height / 15, null);
+				else {
+					System.out.println("THIS SHIT IS NULL NIGGA");
+				}
+			}
+		}
+
+		for (int i = 0; i < currentBoard.length; i++) {
+			for (int j = -1; j < currentBoard[0].length - 1; j++) {
+				if (currentObjects[currentObjects[0].length - 1 - (j + 1)][i] != null) {
+					double x = (i * 0.5 * width / currentBoard.length)
+							- (j * 0.5 * width / currentBoard.length)
+							+ (currentBoard.length / 1.75)
+							* (int) width
+							/ 13
+							- (0.25 * width / currentBoard.length)
+							- (0.125 * currentObjects[currentObjects[0].length - 1 - (j + 1)][i].imgs[3]
+									.getWidth(null) * (width / startingWidth));
+					double y = (i * 0.5 * height / (currentBoard[0].length * 1.5))
+							+ (j * 0.5 * height / (currentBoard[0].length * 1.5))
+							+ (currentBoard[0].length / 3)
+							* (int) height
+							/ 21
+							+ (0.25 * height / currentBoard[0].length * 1.5)
+							- (currentObjects[currentObjects[0].length - 1 - (j + 1)][i].imgs[0].getHeight(null)
+									* (height / startingHeight) - 0.5
+									* currentBoard[i][j + 1].img
+											.getHeight(null)
+									* (height / startingHeight));
+					System.out.println(currentObjects[currentObjects[0].length - 1 - (j + 1)][i]);
+					g.drawImage(
+							currentObjects[currentObjects[0].length - 1 - (j + 1)][i].imgs[3],
+							(int) x,
+							(int) y,
+							(int) (currentObjects[currentObjects[0].length - 1 - (j + 1)][i].imgs[0].getWidth(null) * (width / startingWidth)),
+							(int) (currentObjects[currentObjects[0].length - 1 - (j + 1)][i].imgs[0].getHeight(null) * (height / startingHeight)),
+							null);
+				}
+			}
+		}
+	}
 }
