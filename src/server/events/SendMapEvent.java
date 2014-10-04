f990cef6bec872768467ee3d7652dda4575bed3c
Bryden Frizzell <frizzebryd@the-meeting-tree.ecs.vuw.ac.nz>
2014-10-04 13:40:43 +1300

    Wrote SendMapEvent write function

--- src/server/events/SendMapEvent.java
+++ src/server/events/SendMapEvent.java
@@ -1,18 +1,28 @@
 package server.events;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 public class SendMapEvent implements UpdateEvent {
 
-	public SendMapEvent() {
+	private byte[] data;
 
+	public SendMapEvent(byte[] data) {
+		this.data = data;
 	}
 
 	@Override
 	public void writeTo(OutputStream out) throws IOException {
-		// TODO Auto-generated method stub
-
+		out.write(0x07);
+		out.write(data.length);
+		out.write(data.length>>8);
+		out.write(data.length>>16);
+		out.write(data.length>>24);
+		for(int i=0;i<data.length;i++) {
+			out.write(data[i]);
+		}
 	}
 
 }
