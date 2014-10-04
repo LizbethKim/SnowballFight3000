018f7df4cad17a01843538b610b6d0ff484d6ea6
Kelsey Jack <jackkels@prefab.ecs.vuw.ac.nz>
2014-10-04 15:23:05 +1300

    Default board of different size for singlePlayerGame.

--- src/gameworld/world/Board.java
+++ src/gameworld/world/Board.java
@@ -20,23 +20,26 @@ public class Board {
 	}
 
 	public Board() {
-		board = new Tile[10][10];
-		for (int x = 1; x < 9; x++) {
+		board = new Tile[15][20];
+		for (int x = 1; x < 14; x++) {
 			board[x][0] = new Tile(new Location(x, 0), Terrain.GRASS, null);
-			board[x][9] = new Tile(new Location(x, 9), Terrain.GRASS, null);
-			for (int y = 1; y < 9; y++) {
+			board[x][19] = new Tile(new Location(x, 19), Terrain.GRASS, null);
+			for (int y = 1; y < 19; y++) {
 				board[x][y] = new Tile(new Location(x, y), Terrain.SNOW, null);
 			}
 		}
-		for (int y = 0; y < 10; y++) {
+		for (int y = 0; y < 20; y++) {
 			board[0][y] = new Tile(new Location(0, y), Terrain.GRASS, null);
-			board[9][y] = new Tile(new Location(9, y), Terrain.GRASS, null);
+			board[14][y] = new Tile(new Location(14, y), Terrain.GRASS, null);
 		}
 
 		board[0][0].place(new Furniture("A tree", Objects.TREE));
 		board[4][2].place(new Furniture("A tree", Objects.TREE));
 		board[5][3].place(new Furniture("A tree", Objects.TREE));
 		board[2][7].place(new Furniture("A bush", Objects.BUSH));
+		board[12][17] = new Tile(new Location(12,17), Terrain.GRASS, null);
+		board[13][10].place(new Furniture("A bush", Objects.BUSH));
+
 	}
 
 	public Tile tileAt(Location l) {
