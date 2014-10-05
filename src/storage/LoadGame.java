3fc851b1e7f28930ce248c5cdb550b4c52fa7232
Kate Henderson <kate.henderson.nz@gmail.com>
2014-10-06 00:47:36 +1300

    New Parser using different library
    Still need to remove old loader, haven't fully tested yet.

--- src/storage/LoadGame.java
+++ src/storage/LoadGame.java
@@ -4,63 +4,28 @@
 package storage;
 
 import java.io.File;
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.SAXException;
-
-import storage.load.LoadHandler;
+import storage.load.StaxParser;
 
 /**
- * Loads a previously saved game using the SAX library and XML.
+ * Loads a previously saved game using the StAX library and XML.
  * @author Kate Henderson 300279254
  *
  */
 public class LoadGame{
 	private StoredGame game;
 
-	public LoadGame(){
-
-	}
-
-	/**
-	 * Purely for testing, will be phased out when some kind of file chooser is done. Possibly just a default to the
-	 *  last saved game, and then a pre-established one to demonstrate loading
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-	    try {
-	        SAXParser saxParser = saxParserFactory.newSAXParser();
-	        LoadHandler handler = new LoadHandler();
-	        saxParser.parse(new File("src/storage/defaultBoard.xml"), handler);
-
-	        StoredGame testGame = handler.buildGame();
-	        System.out.println("Game Loaded by loader");
-
-	    } catch (ParserConfigurationException | SAXException | IOException e) {
-	        e.printStackTrace();
-	    }
+		
+		StaxParser parser = new StaxParser();
+		StoredGame testGame = parser.parse(new File("src/storage/defaultBoard.xml"));
+        System.out.println("Game Loaded by loader");
 	}
-
+	
 	public StoredGame loadGame(String filename){
-		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-	    try {
-	        SAXParser saxParser = saxParserFactory.newSAXParser();
-	        LoadHandler handler = new LoadHandler();
-	        saxParser.parse(new File("src/storage/"+filename), handler);
-
-	        game = handler.buildGame();
-	        System.out.println("Game Loaded by main");
-
-	    } catch (ParserConfigurationException | SAXException | IOException e) {
-	        e.printStackTrace();
-	    }
+		StaxParser parser = new StaxParser();
+		game = parser.parse(new File("src/storage/defaultBoard.xml"));
+		System.out.println("Game Loaded by loader");
 		return game;
-
 	}
 
 	public StoredGame loadGame(Byte[] map) {
---
+++ src/storage/load/StaxParser.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package storage.load;
+
+import gameworld.world.Board;
+import gameworld.world.Furniture;
+import gameworld.world.InanimateEntity;
+import gameworld.world.Location;
+import gameworld.world.Player;
+import gameworld.world.Team;
+import gameworld.world.Tile;
+import graphics.assets.Objects;
+import graphics.assets.Terrain;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+import storage.StoredGame;
+
+
+
+/**
+ * Handles the parsing of the XML file using StAX into a board and player list, ready to be loaded in game
+ * @author Kate Henderson
+ *
+ */
+
+public class StaxParser {
+	static final String GAME = "game";
+	static final String PLAYERS = "players";
+	static final String PLAYER = "player";
+	static final String BOARD = "board";
+	static final String TILE = "tile";
+	static final String INVENTORY= "inventory";
+	
+	static final String DELIMITER = "\\s+";
+
+	private Board board;
+	private Tile[][] tiles;
+	private List<Player> players;
+	private Player curPlayer;
+	private Tile curTile;
+
+	/**
+	 * @param file
+	 */
+	public StoredGame parse(File file) {	
+		try {
+			// XMLInputFactory, used to process input
+			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+			// Starts streaming the file
+			InputStream in = new FileInputStream(file);
+			//Make the event reader to detect the XML
+			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+
+			while (eventReader.hasNext()) {
+				XMLEvent event = eventReader.nextEvent();
+
+				if (event.isStartElement()) {
+					StartElement startElement = event.asStartElement();
+					String elemName = startElement.getName().getLocalPart();
+					
+					if (elemName.equals(GAME)) {
+						//Could add Name of the game or something here
+						continue;
+					}
+					
+					if (elemName.equals(PLAYERS)) {
+						players = new ArrayList<>();
+						continue;
+					}
+					
+					if (elemName.equals(PLAYER)) {
+						event = eventReader.nextEvent();
+						String[] values = event.asCharacters().getData().split(DELIMITER);
+						int teamNum = Integer.parseInt(values[0]);
+						Team team = Team.values()[teamNum];
+						Location loc = parseLoc(values[1], values[2]);
+						curPlayer = new Player(team, loc, values[3]);
+						continue;
+					}
+
+					if (elemName.equals(BOARD)) {
+						event = eventReader.nextEvent();
+						String[] values = event.asCharacters().getData().split(DELIMITER);
+						Location boardSize = parseLoc(values[0],values[1]);
+						tiles = new Tile[boardSize.x][boardSize.y];
+						continue;
+					}
+					
+					if (elemName.equals(TILE)) {
+						event = eventReader.nextEvent();
+						String[] values = event.asCharacters().getData().split(DELIMITER);
+						int terrain = Integer.parseInt(values[0]);
+						Location loc = parseLoc(values[1],values[2]);
+						InanimateEntity entity = null;
+						if(values.length>3){	//there is something on the tile
+							int obj = Integer.parseInt(values[3]);
+							
+							if (obj==4)
+								entity = new Furniture("default tree", Objects.values()[4]);
+						}			
+						curTile = new Tile(loc,Terrain.values()[terrain], entity);
+						continue;
+					}
+
+					if (elemName.equals(INVENTORY)) { 	//not implemented yet
+						/*if(eventReader.peek().isCharacters()){	//Inventory could be empty, so check if anything is there
+							event = eventReader.nextEvent();
+							String[] values = event.asCharacters().getData().split(DELIMITER);
+						}*/
+						continue;
+					}
+				
+				}
+				
+				if (event.isEndElement()) {
+					EndElement endElement = event.asEndElement();
+					String elemName = endElement.getName().getLocalPart();
+					if (elemName.equals(PLAYER)) {
+						players.add(curPlayer);
+						continue;
+					}
+					if (elemName.equals(GAME)) {
+						System.out.println("GAME LOADED");
+						break;
+					}
+					if (elemName.equals(TILE)) {
+						tiles[curTile.getCoords().x][curTile.getCoords().y] = curTile;
+						continue;
+					}
+				}
+
+			}
+		} catch (FileNotFoundException | XMLStreamException e) {
+			e.printStackTrace();
+		}
+
+		return new StoredGame(board, players);
+		
+	}
+
+	private Location parseLoc(String xStr, String yStr){
+		int x = Integer.parseInt(xStr);
+		int y = Integer.parseInt(yStr);
+		return new Location(x, y);
+	}
+} 
