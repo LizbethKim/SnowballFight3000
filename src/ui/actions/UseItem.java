86aea3824b669ec0e2aab9b47ca463d794b293f1
RyanBurnell <ryan.burnell2@gmail.com>
2014-10-12 21:34:21 +1300

    added code for opening a container from the inventory

--- src/ui/actions/UseItem.java
+++ src/ui/actions/UseItem.java
@@ -1,18 +1,34 @@
 package ui.actions;
+
+import java.util.List;
+
+import ui.ContainerPopup;
 import ui.UI;
 import gameworld.game.client.ClientGame;
+import gameworld.game.client.NoItemException;
+import gameworld.game.client.NotAContainerException;
 import gameworld.world.Board;
+import graphics.assets.Objects;
 
+public class UseItem extends KeyAction {
 
-public class UseItem extends KeyAction{
-
-	public UseItem(ClientGame cl, UI parent){
+	public UseItem(ClientGame cl, UI parent) {
 		super(cl, parent);
 	}
-	
+
 	@Override
 	protected void execute() {
+		if (client.selectedIsContainer()) {
+			try {
+				List<Objects> items = client.getContentsOfSelected();
+				String title = client.inspectItem();
+				new ContainerPopup(client, parent, title, items, enabled);
+			} catch (NotAContainerException | NoItemException e) {
+				// do nothing
+			}
+		} else {
 		client.useItem();
+		}
 	}
 
 }
