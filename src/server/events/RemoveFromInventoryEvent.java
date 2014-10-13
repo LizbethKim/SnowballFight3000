061e895eb7ebcd67ae9369b71ece3ccbb94229df
Bryden Frizzell <frizzebryd@cafe-india.ecs.vuw.ac.nz>
2014-10-13 17:18:33 +1300

    Added removeFromInventoryEvent

--- src/server/events/RemoveFromInventoryEvent.java
+++ src/server/events/RemoveFromInventoryEvent.java
@@ -1,17 +1,22 @@
 package server.events;
 
+import gameworld.world.Location;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class RemoveFromInventoryEvent implements UpdateEvent {
+public class RemoveFromInventoryEvent extends LocationEvent {
 	private int index;
-	public RemoveFromInventoryEvent(int index){
+	private Location location;
+	public RemoveFromInventoryEvent(Location l, int index){
 		this.index=index;
+		this.location=l;
 	}
 	@Override
 	public void writeTo(OutputStream out) throws IOException {
 		// TODO Auto-generated method stub
 		out.write(0x10);
+		writeLocation(out, location);
 		out.write(index);
 	}
 
