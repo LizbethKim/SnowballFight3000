aa4d4ed9f4bfe4d6bd5303250d96105d41655bc9
Kelsey Jack <jackkels@myvuw.ac.nz>
2014-10-13 18:30:52 +1300

    Added a FlagTile, intended to be where you place the other team's flag to win

---
+++ src/gameworld/world/FlagTile.java
@@ -0,0 +1,21 @@
+package gameworld.world;
+
+import graphics.assets.Terrain;
+
+public class FlagTile extends Tile {
+	private Team t;
+	public FlagTile(Location c, Terrain type, Team t) {
+		super(c, type, null);
+		this.t = t;
+	}
+
+	@Override
+	public boolean place (InanimateEntity on) {
+		if (on instanceof Flag && ((Flag)on).getTeam() == t) {
+			// KTC win the game?
+			return super.place(on);
+		}
+		return false;
+	}
+
+}
