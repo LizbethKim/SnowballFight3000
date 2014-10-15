f28480ebc2c1eb3aeb87586140e266b2bfda47ef
Bryden Frizzell <frizzebryd@pie-cart.ecs.vuw.ac.nz>
2014-10-15 15:53:14 +1300

    Added Javadoc and removed debug code

--- src/network/SocketClosedException.java
+++ src/network/SocketClosedException.java
@@ -1,5 +1,10 @@
 package network;
 
+/**
+ * Throw this when a socket is closed and creating problems
+ * @author Bryden Frizzell
+ *
+ */
 public class SocketClosedException extends Exception {
-	
+
 }
